9a521572578757edee6240f837bf003f2faaded7
illia841 <illiaholodniak@gmail.com>
2022-05-16 00:35:29 +0300

    v1

---
+++ main.py
@@ -0,0 +1,89 @@
+import time
+import os
+import random
+from colorama import init, Fore
+import webbrowser
+
+init()
+
+#RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+
+reklamaImage = ["images/index.jpg", "images/indssex.jpg", "images/imaвges.jpg", "images/maxresdefault.jpg", "images/images.jpg"]
+reklamaURl = ["https://haircutexpress.eu/ru/", "https://rt.pornhub.com", "https://abcfood.net/ru/product-category/hren-gorchica/hren/", "https://oldspice.com/", "https://www.tavro-kozha.ru/catalog/all/"]
+x = 1
+
+while True:
+	os.system('cls||clear')
+
+	y = x % 5
+
+	x += 1
+
+	if y == 0:
+		j = random.randrange(5)
+
+		os.startfile(reklamaImage[j])
+
+		print("5...")
+		time.sleep(1)
+		print("4...")
+		time.sleep(1)
+		print("3...")
+		time.sleep(1)
+		print("2...")
+		time.sleep(1)
+		print("1...")
+		time.sleep(1)
+		os.system('cls||clear')
+
+		print('\n\n       1-skip           2-перейти')
+		try:
+			reklamaInput = int(input("\n\n       Сделай выбор: "))
+		except:
+			print("Ты даун ЕБУЧИЙ")
+
+		while True:
+			if reklamaInput == 1:
+				break
+			elif reklamaInput == 2:
+				webbrowser.open(reklamaURl[j], new=0, autoraise = False)
+				input()
+				break
+			else:
+				print("ТЫ ДАУН ЕБУЧИЙ")
+				continue
+		
+
+	else:
+		print(Fore.RED, '\n\n\n     ██╗  ███████╗██╗░░░██╗░█████╗░██╗░░██╗  ██╗░░░██╗░█████╗░██╗░░░██╗██████╗░')
+		time.sleep(0.2)
+		print(Fore.GREEN,'    ██║  ██╔════╝██║░░░██║██╔══██╗██║░██╔╝  ╚██╗░██╔╝██╔══██╗██║░░░██║██╔══██╗')
+		time.sleep(0.2)
+		print(Fore.YELLOW,'    ██║  █████╗░░██║░░░██║██║░░╚═╝█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.BLUE,'    ██║  █████╗░░██║░░░██║██║░░╚═╝█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.MAGENTA,'    ██║  █████╗░░██║░░░██║██║░░╚═╝█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.CYAN,'    ██║  █████╗░░██║░░░██║██║░░╚═╝█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.RESET,'    ██║  █████╗░░██║░░░██║██║░░╚═╝█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.RED,'    ██║  ██║░░░░░╚██████╔╝╚█████╔╝██║░╚██╗  ░░░██║░░░╚█████╔╝╚██████╔╝██║░░██║')
+		time.sleep(0.2)
+		print(Fore.GREEN,'    ╚═╝  ╚═╝░░░░░░╚═════╝░░╚════╝░╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚═════╝░╚═╝░░╚═╝')
+		time.sleep(0.2)
+		print('')
+		time.sleep(0.2)
+		print(Fore.YELLOW,'    ░██████╗████████╗███████╗██████╗░██████╗░░█████╗░██████╗░')
+		time.sleep(0.2)
+		print(Fore.BLUE,'    ██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗')
+		time.sleep(0.2)
+		print(Fore.MAGENTA,'    ╚█████╗░░░░██║░░░█████╗░░██████╔╝██║░░██║███████║██║░░██║')
+		time.sleep(0.2)
+		print(Fore.CYAN,'    ░╚═══██╗░░░██║░░░██╔══╝░░██╔═══╝░██║░░██║██╔══██║██║░░██║')
+		time.sleep(0.2)
+		print(Fore.RESET,'    ██████╔╝░░░██║░░░███████╗██║░░░░░██████╔╝██║░░██║██████╔╝')
+		time.sleep(0.2)
+		print(Fore.RED,'    ╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═════╝░╚═╝░░╚═╝╚═════╝░')
+		time.sleep(2)
